9a9416d8913ad18b765232b43c7f5bee6086d4d4
kiyusama <kiyusama328@gmail.com>
2023-10-27 09:51:48 +0900

    add a css file

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+        <div className="logo">
+            <h3>ポートフォリオ</h3>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">ホーム</a>
+                </li>
+                <li>
+                    <a href="#">ブログ</a>
+                </li>
+                <li>
+                    <a href="#">SNS</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
