52eed209f6f21367f50b99de701b6ca0739cd614
kiyusama <kiyusama328@gmail.com>
2023-10-28 23:20:59 +0900

    add hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
-import { Navbar, Nav, Button} from 'react-bootstrap';
+import { Navbar, Nav} from 'react-bootstrap';
+import top_img from '../assets/top.png';
+
 
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" fixed="top" expand="lg">
-      <Navbar.Brand href="#">ポートフォリオ3</Navbar.Brand>
-      <Nav className="ml-auto">
-        <Nav.Link href="#">ホーム</Nav.Link>
-        <Nav.Link href="#">ブログ</Nav.Link>
-        <Nav.Link href="#">SNS</Nav.Link>
-
-        <Button className="bi bi-alarm"></Button>
-        
-      </Nav>
+      <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src={top_img}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}ポートフォリオ
+      </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#">ホーム</Nav.Link>
+            <Nav.Link href="#">ブログ</Nav.Link>
+            <Nav.Link href="#">SNS</Nav.Link>
+            <span className="nav-link bi bi-alarm mr-2"></span>
+          </Nav>
+        </Navbar.Collapse>
     </Navbar>
   );
 }
 
-export default Header
+export default Header;
