29a15abf1f0843bf0bddaecccb050d59d232a911
kiyusama <kiyusama328@gmail.com>
2023-10-29 23:24:52 +0900

    success parallax

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,38 @@
-import React from 'react'
-import { Container, Row ,Col } from 'react-bootstrap'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Home = () => {
   return (
     <div>
-        
-      
-        <div  className='backHome d-flex align-items-center'>
-          <Container>
-            <Row>
-              <Col lg={7} className='mx-auto'>
-                  <h1>Home5</h1>
-                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus quod delectus nostrum, hic dolorum iusto inventore ullam aut similique qui perferendis minima! Porro, ad? Natus, omnis? Nemo saepe placeat commodi?</p>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      
-
-
-
+      <div className='backHome d-flex align-items-center'>
+        <div className='bg-img'></div>
+        <Container>
+          <Row>
+            <Col lg={7} className='mx-auto'>
+              <h1>Home5</h1>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus quod delectus nostrum, hic dolorum iusto inventore ullam aut similique qui perferendis minima! Porro, ad? Natus, omnis? Nemo saepe placeat commodi?</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
       <Container className='vh-100 d-flex align-items-center'>
         <Row>
           <Col lg={7} className='mx-auto'>
-              <h1>Home</h1>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus quod delectus nostrum, hic dolorum iusto inventore ullam aut similique qui perferendis minima! Porro, ad? Natus, omnis? Nemo saepe placeat commodi?</p>
+            <h1>Home</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus quod delectus nostrum, hic dolorum iusto inventore ullam aut similique qui perferendis minima! Porro, ad? Natus, omnis? Nemo saepe placeat commodi?</p>
           </Col>
         </Row>
       </Container>
-
       <Container className='vh-100 d-flex align-items-center'>
         <Row>
           <Col lg={7} className='mx-auto'>
-              <h1>Home</h1>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus quod delectus nostrum, hic dolorum iusto inventore ullam aut similique qui perferendis minima! Porro, ad? Natus, omnis? Nemo saepe placeat commodi?</p>
+            <h1>Home</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus quod delectus nostrum, hic dolorum iusto inventore ullam aut similique qui perferendis minima! Porro, ad? Natus, omnis? Nemo saepe placeat commodi?</p>
           </Col>
         </Row>
       </Container>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
