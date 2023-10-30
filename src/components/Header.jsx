70cb2a4bc3c6ca5513e97ac434d84bb4085721a3
kiyusama <kiyusama328@gmail.com>
2023-10-30 13:04:21 +0900

    before add router

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,8 +20,8 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#">ホーム</Nav.Link>
-            <Nav.Link href="#">ブログ</Nav.Link>
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">About</Nav.Link>
             <Nav.Link href="#">SNS</Nav.Link>
             <span className="nav-link bi bi-alarm mr-2"></span>
           </Nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,18 @@ import { Container, Row, Col } from 'react-bootstrap';
 const Home = () => {
   return (
     <div>
-      <div className='backHome d-flex align-items-center'>
-        <div className='bg-img'></div>
+      <div className='back d-flex align-items-center'>
+        <div className='bg-imgHome'></div>
         <Container>
           <Row>
             <Col lg={7} className='mx-auto'>
-              <h1>Home5</h1>
+              <h1>Home10</h1>
               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus quod delectus nostrum, hic dolorum iusto inventore ullam aut similique qui perferendis minima! Porro, ad? Natus, omnis? Nemo saepe placeat commodi?</p>
             </Col>
           </Row>
         </Container>
       </div>
+
       <Container className='vh-100 d-flex align-items-center'>
         <Row>
           <Col lg={7} className='mx-auto'>
