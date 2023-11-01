b115a0fb84bfd44094d228eea9f5d3b356eec223
kiyusama <kiyusama328@gmail.com>
2023-11-01 10:26:29 +0900

    add useState

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, Container} from 'react-bootstrap';
 import topImg from '../assets/images/top.png';
 import { Link } from 'react-router-dom';
@@ -6,8 +6,11 @@ import { Link } from 'react-router-dom';
 
 
 const Header = () => {
+
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar className="bg-body-tertiary" expand="lg" bg="dark" data-bs-theme="dark" fixed="top">
+    <Navbar className="bg-body-tertiary" expand="lg" bg="dark" data-bs-theme="dark" fixed="top" expanded={expanded} onToggle={() => setExpanded(!expanded)}>
       <Container>
         <Navbar.Brand href="#">
           <img
@@ -21,9 +24,9 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/about" className="nav-link">About</Link>
-            <Link to="/history" className="nav-link">History</Link>
+            <Link to="/" className="nav-link" onClick={() => setExpanded(false)}>Home</Link>
+            <Link to="/about" className="nav-link" onClick={() => setExpanded(false)}>About</Link>
+            <Link to="/history" className="nav-link" onClick={() => setExpanded(false)}>History</Link>
             
           </Nav>
         </Navbar.Collapse>
